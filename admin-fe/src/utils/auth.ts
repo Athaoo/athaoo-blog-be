53569f53d7204bc77e29ef8a55c26c3df8ddccf9
lijinchao <13522655053@163.com>
2023-07-27 17:58:41 +0800

    refractor: 移除admin-fe

--- admin-fe/src/utils/auth.ts
+++
@@ -1,9 +0,0 @@
-export const isLogin = () => {
-  const token = getToken()
-  if (typeof token == 'string' && token.length) return true
-  return false
-}
-
-export const getToken = () => {
-  return window.sessionStorage.getItem('token')
-}
