a394bedb8d226dd27cf35db2ae801e1cf7a49d5f
阿骚 <2820911435@qq.com>
2023-07-30 23:55:52 +0800

    doc: 1

--- be/src/database/config.js
+++ be/src/database/config.js
@@ -1,5 +1,16 @@
+const ip = '154.8.162.201'
+
+// 线上和生产是两个库
 const config = {
   development: {
+    username: 'athaoo',
+    password: 'athaooblog123',
+    database: 'athaoo_blog',
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql',
+  },
+  production: {
     username: 'athaoo',
     password: 'athaooblog123',
     database: 'athaoo_blog',
