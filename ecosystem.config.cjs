5f5494b5afacb48dc0e632cfd9fa1aad086f9a62
lijinchao <13522655053@163.com>
2023-08-03 14:43:15 +0800

    feat: 加入pm2 config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,15 @@
+const { config } = require('dotenv-flow')
+
+// 读取环境变量
+config({ debug: true })
+
+module.exports = {
+  apps: [
+    {
+      name: 'process.env.APP_NAME' ?? 'blog',
+      script: './app.js',
+      watch: true,
+      ignore_watch: ['node_modules', 'logs'],
+    },
+  ],
+}
--- src/database/config.js
+++ src/database/config.js
@@ -1,8 +1,3 @@
-import { config } from 'dotenv-flow'
-
-// 读取环境变量
-config({ debug: true })
-
 const dbConfig = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
